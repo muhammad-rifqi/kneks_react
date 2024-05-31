eb56910f75d8b2659e9267c1aa0b7c09cb2fd2a1
Muhammad Rifqi <ats.muhammadr@xl.co.id>
2024-05-31 14:42:23 +0700

    aaa

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -115,7 +115,7 @@ const Footer = () => {
         <div className="search-popup-overlay search-toggler"></div>
         <div className="search-popup-content">
           <form action="#">
-            <label for="search" className="sr-only">search here</label>
+            <label htmlFor="search" className="sr-only">search here</label>
             <input type="text" id="search" placeholder="Search Here..." />
             <button type="submit" aria-label="search submit" className="search-btn">
               <span><i className="flaticon-search-interface-symbol"></i></span>
