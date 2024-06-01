32fc3863c8ff9b3b344a62079a52edd82a6ff218
Rizsky Darmawan <darmawanrizsky@gmail.com>
2024-06-01 23:46:55 +0700

    update
    direktorat

--- src/component/home/Section2.js
+++ src/component/home/Section2.js
@@ -8,10 +8,30 @@ const Section2 = () => {
                         <div className="col-xl-2 col-lg-4 col-md-6">
                             <div className="department-card">
                                 <div className="department-card-icon">
+                                    <a href="departments.html"><i className="flaticon-lotus"></i></a>
+                                </div>
+                                <div className="department-card-content">
+                                    <h5><a href="department-details.html">Direktorat Industri Produksi Halal</a></h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-xl-2 col-lg-4 col-md-6">
+                            <div className="department-card">
+                                <div className="department-card-icon">
+                                    <a href="departments.html"><i className="flaticon-balance-1"></i></a>
+                                </div>
+                                <div className="department-card-content">
+                                    <h5><a href="department-details.html">Direktorat Jasa Keuangan Syariah</a></h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-xl-2 col-lg-4 col-md-6">
+                            <div className="department-card">
+                                <div className="department-card-icon">
                                     <a href="departments.html"><i className="flaticon-parthenon"></i></a>
                                 </div>
                                 <div className="department-card-content">
-                                    <h5><a href="department-details.html">Your Government</a></h5>
+                                    <h5><a href="department-details.html">Direktorat Keuangan Sosial Syariah</a></h5>
                                 </div>
                             </div>
                         </div>
@@ -21,7 +41,7 @@ const Section2 = () => {
                                     <a href="departments.html"><i className="flaticon-suitcase"></i></a>
                                 </div>
                                 <div className="department-card-content">
-                                    <h5><a href="department-details.html">Jobs & Unemployment</a></h5>
+                                    <h5><a href="department-details.html">Direktorat Ekosistem Syariah</a></h5>
                                 </div>
                             </div>
                         </div>
@@ -32,49 +52,14 @@ const Section2 = () => {
                                     <a href="departments.html"><i className="flaticon-industry"></i></a>
                                 </div>
                                 <div className="department-card-content">
-                                    <h5><a href="department-details.html">Business & Industry</a></h5>
+                                    <h5><a href="department-details.html">Direktorat Kewirausahaan Syariah</a></h5>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-xl-2 col-lg-4 col-md-6">
-                            <div className="department-card">
-                                <div className="department-card-icon">
-                                    <a href="departments.html"><i className="flaticon-bus"></i></a>
-                                </div>
-                                <div className="department-card-content">
-                                    <h5><a href="department-details.html">Roads & Transport</a></h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-xl-2 col-lg-4 col-md-6">
-                            <div className="department-card">
-                                <div className="department-card-icon">
-                                    <a href="departments.html"><i className="flaticon-lotus"></i></a>
-                                </div>
-                                <div className="department-card-content">
-                                    <h5><a href="department-details.html">Culture & Recreation</a></h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-xl-2 col-lg-4 col-md-6">
-                            <div className="department-card">
-                                <div className="department-card-icon">
-                                    <a href="departments.html"><i className="flaticon-balance-1"></i></a>
-                                </div>
-                                <div className="department-card-content">
-                                    <h5><a href="department-details.html">Justice, Safety Law</a></h5>
-                                </div>
-                            </div>
-                        </div>
+
+
+
                     </div>
-                </div>
-            </div>
-            <div className="department-search-section">
-                <div className="container">
-                    <form className="department-search-form">
-                        <input type="text" placeholder="Get our quick services from the city municipal" name="search" />
-                        <button type="submit">View All Services</button>
-                    </form>
                 </div>
             </div>
         </section>
--- src/component/home/Section3.js
+++ src/component/home/Section3.js
@@ -7,52 +7,27 @@ const Section3 = () => {
                 <div className="row justify-content-between">
                     <div className="col-lg-6">
                         <div className="about-image">
-                            <div className="about-image-inner img-one">
+                            {/* <div className="about-image-inner img-one">
                                 <img src="/assets/image/gallery/about-1.jpg" className="img-fluid" alt="img-2" />
-                                <div className="sign-text">Kevin Martin</div>
-                                <div className="about-image-caption">
-                                    <div className="about-image-caption-inner">
-                                        <span className="about-caption-number">18</span>
-                                        <span className="about-caption-text">Years of<br />experience</span>
-                                    </div>
-                                </div>
-                            </div>
+
+                            </div> */}
                             <div className="about-image-inner img-two">
                                 <img src="/assets/image/shapes/about-3.jpg" className="floated-image" alt="img-3" />
-                                <img src="/assets/image/gallery/about-2.jpg" className="img-fluid" alt="img-4" />
+                                <img src="/assets/image/gallery/mayor-2.jpg" className="img-fluid" alt="img-4" />
                             </div>
                         </div>
                     </div>
                     <div className="col-lg-6">
                         <div className="about-inner">
                             <div className="section-title-box">
-                                <div className="section-tagline">Our introductions</div>
-                                <h2 className="section-title">Welcome & Support to City Municipal</h2>
-                                <p>Aliquam viverra arcu. Donec aliquet blandit enim feugiat. Suspendisse id quam sed eros tincidunt luctus sit amet eu nibh egestas tempus turpis, sit amet mattis magna varius non.</p>
+                                <div className="section-tagline">About Us</div>
+                                <h2 className="section-title">Komite Nasional Ekonomi dan Keuangan Syariah (KNEKS)</h2>
+                                <p>merupakan perubahan dari KNKS untuk peningkatan pembangunan ekosistem ekonomi dan keuangan syariah serta menjadikan Indonesia sebagai Pusat Halal Dunia.  Pencanangan titik awal untuk memposisikan Indonesia sebagai salah satu pelaku utama dan hub ekonomi syariah dunia dilakukan seiring dengan peluncuran Masterplan Ekonomi Syariah Indonesia pada bulan Mei 2019.</p>
+
+
                             </div>
-                            <div className="row">
-                                <div className="col-xl-6 col-lg-12 col-md-6">
-                                    <div className="about-card">
-                                        <h4 className="about-title"><i className="fa-solid fa-circle-check"></i>Business & Economy</h4>
-                                        <p className="about-text">Lorem ipsum dolor sited amet consectetur notted.</p>
-                                    </div>
-                                </div>
-                                <div className="col-xl-6 col-lg-12 col-md-6">
-                                    <div className="about-card">
-                                        <h4 className="about-title"><i className="fa-solid fa-circle-check"></i>Health & Education</h4>
-                                        <p className="about-text">Lorem ipsum dolor sited amet consectetur notted.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="about-author-box">
-                                <div className="about-author-image">
-                                    <img src="/assets/image/gallery/about-4.png" className="img-fluid" alt="img-5" />
-                                </div>
-                                <div className="about-author-box-meta">
-                                    <h5>Mr. Kevin Martin</h5>
-                                    <span>City Mayor</span>
-                                </div>
-                            </div>
+
+
                         </div>
                     </div>
                 </div>
