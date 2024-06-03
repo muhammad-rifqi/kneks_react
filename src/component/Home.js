03e3f56e8555581e08038dffcf4f712374db5fe7
Rizsky Darmawan <darmawanrizsky@gmail.com>
2024-06-03 19:42:48 +0700

    Update Home.js
    update

--- src/component/Home.js
+++ src/component/Home.js
@@ -26,15 +26,17 @@ const Home = () => {
       <Section2 />
       <Section3 />
       <Section14 />
-      <Section4 />
       <Section5 />
+      <Section12 />
+      <Section4 />
+
       <Section6 />
       <Section7 />
       <Section8 />
       <Section9 />
       <Section10 />
       <Section11 />
-      <Section12 />
+
       <Section13 />
     </div>
 
