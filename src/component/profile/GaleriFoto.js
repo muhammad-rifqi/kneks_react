5e42dfbcc9f631005faf023aa28480ec69a759b5
Rizsky Darmawan <darmawanrizsky@gmail.com>
2024-06-15 16:30:18 +0700

    foto dan video
    updte

--- src/component/profile/GaleriFoto.js
+++ src/component/profile/GaleriFoto.js
@@ -1,52 +1,135 @@
 import React from "react";
+
+
+
+
+
 const GaleriFoto = () => {
+
+
     return (
         <>
-           <div className="page-wrapper">
-            
-            <section className="page-banner">
-                <div className="container">
-                    <div className="page-banner-title">
-                        <h3>Galeri Foto</h3>
+            <div className="page-wrapper">
+
+                <section className="page-banner">
+                    <div className="container">
+                        <div className="page-banner-title">
+                            <h3>Galeri Foto</h3>
+                        </div>
                     </div>
-                </div>
-            </section>
-            <section className="about-one-section">
-                <div className="container">
-                    <div className="row row-gutter-y-40">
-                        <div className="col-lg-12 col-xl-6">
-                            <div className="about-one-inner">
-                                <div className="section-tagline">
-                                    Our introductions
-                                </div>
-                                <h2 className="section-title">Welcome to Mexican City Municipal Council</h2>
-                                <p>Aliquam viverra arcu. Donec aliquet blandit enim feugiat. Suspendisse id quam sed eros tincidunt luctus sit amet eu nibh egestas tempus turpis, sit amet mattis magna varius non.</p>
-                                <h5 className="about-one-inner-text">Denounce with righteous indignation and dislike men who are so beguiled & demoralized our power.</h5>
-                                <div className="row row-gutter-y-30">
-                                    <div className="col-xl-6 col-lg-6 col-md-6">
-                                        <div className="about-one-card">
-                                            <div className="about-one-card-number">01</div>
-                                            <div className="about-one-card-content"><h5>Going Above and Beyond</h5></div>
+                </section>
+                <section className="foto-section">
+                    <div className="container">
+                        <div className="row row-gutter-y-40">
+                            <div className="col-md-4 col-lg-4">
+                                <div className="card-box-b card-shadow news-box">
+                                    <div className="img-box-b " data-gall="gallery01">
+                                        <img src="/assets/image/berita2.jpeg" alt="imgNews" className="img-b img-fluid" />
+                                    </div>
+                                    <div className="card-overlay">
+                                        <div className="card-header-b">
+
+                                            <div className="card-title-b">
+                                                <h2 className="title-2">
+                                                    <a href="blog-single.html">Travel is comming
+                                                        new</a>
+                                                </h2>
+                                            </div>
+                                            <div className="card-date">
+                                                <span className="date-b">18 Sep. 2017</span>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div className="col-xl-6 col-lg-6 col-md-6">
-                                        <div className="about-one-card">
-                                            <div className="about-one-card-number">02</div>
-                                            <div className="about-one-card-content"><h5>Committed to People First</h5></div>
+                                </div>
+                            </div>
+                            <div className="col-md-4 col-lg-4">
+                                <div className="card-box-b card-shadow news-box">
+                                    <div className="img-box-b">
+                                        <img src="/assets/image/berita.jpg" alt="imgNews" className="img-b img-fluid" />
+                                    </div>
+                                    <div className="card-overlay">
+                                        <div className="card-header-b">
+
+                                            <div className="card-title-b">
+                                                <h2 className="title-2">
+                                                    <a href="blog-single.html">Travel is comming
+                                                        new</a>
+                                                </h2>
+                                            </div>
+                                            <div className="card-date">
+                                                <span className="date-b">18 Sep. 2017</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-4 col-lg-4">
+                                <div className="card-box-b card-shadow news-box">
+                                    <div className="img-box-b">
+                                        <img src="/assets/image/berita.jpg" alt="imgNews" className="img-b img-fluid" />
+                                    </div>
+                                    <div className="card-overlay">
+                                        <div className="card-header-b">
+
+                                            <div className="card-title-b">
+                                                <h2 className="title-2">
+                                                    <a href="blog-single.html">Travel is comming
+                                                        new</a>
+                                                </h2>
+                                            </div>
+                                            <div className="card-date">
+                                                <span className="date-b">18 Sep. 2017</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-4 col-lg-4">
+                                <div className="card-box-b card-shadow news-box">
+                                    <div className="img-box-b">
+                                        <img src="/assets/image/berita.jpg" alt="imgNews" className="img-b img-fluid" />
+                                    </div>
+                                    <div className="card-overlay">
+                                        <div className="card-header-b">
+
+                                            <div className="card-title-b">
+                                                <h2 className="title-2">
+                                                    <a href="blog-single.html">Travel is comming
+                                                        new</a>
+                                                </h2>
+                                            </div>
+                                            <div className="card-date">
+                                                <span className="date-b">18 Sep. 2017</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-4 col-lg-4">
+                                <div className="card-box-b card-shadow news-box">
+                                    <div className="img-box-b">
+                                        <img src="/assets/image/berita.jpg" alt="imgNews" className="img-b img-fluid" />
+                                    </div>
+                                    <div className="card-overlay">
+                                        <div className="card-header-b">
+
+                                            <div className="card-title-b">
+                                                <h2 className="title-2">
+                                                    <a href="blog-single.html">Travel is comming
+                                                        new</a>
+                                                </h2>
+                                            </div>
+                                            <div className="card-date">
+                                                <span className="date-b">18 Sep. 2017</span>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-lg-12 col-xl-6">
-                            <div className="about-one-image">
-                                <img src="assets/image/shapes/shape-1.png" className="floated-image-one" alt="img-58" />
-                                <img src="assets/image/gallery/about-7.jpg" alt="img-59" className="img-fluid" />
-                            </div>
-                        </div>
                     </div>
-                </div>
-            </section>
+                </section>
+
             </div>
         </>
     )
