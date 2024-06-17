f7fdfb83c0f96188297d51e9b0f438dbd1163efd
Rizsky Darmawan <darmawanrizsky@gmail.com>
2024-06-17 10:58:23 +0700

    sc
    sc

--- src/component/home/Section11.js
+++ src/component/home/Section11.js
@@ -2,74 +2,43 @@ import React from "react"
 import { Link } from "react-router-dom"
 const Section11 = () => {
 	return (
-		<section className="funfact-section">
+		<section className="sm-section">
 			<div className="container">
-				<div className="funfact-box">
+				<div className="sm-box">
 					<div className="section-title-box text-center">
 						<div className="section-tagline-b">Media Social</div>
-						<h2 className="section-title">Instgram Post</h2>
+						<h2 className="section-title">Instagram Post</h2>
 					</div>
 				</div>
 				<div className="row">
-					<div className="col-xl-3 col-md-6">
-						<div className="funfact-counter-item">
-							<div className="funfact-counter-box">
-								<div className="funfact-counter-icon">
-									<i className="flaticon-running-man"></i>
-								</div>
-								<div className="funfact-counter-number">
-									<h3 className="counter-number">84</h3>
-									<span className="funfact-counter-number-postfix">k</span>
-								</div>
-							</div>
-							<p className="funfact-text">Total People Lived<br />in our City</p>
-						</div>
-					</div>
-					<div className="col-xl-3 col-md-6">
-						<div className="funfact-counter-item">
-							<div className="funfact-counter-box">
-								<div className="funfact-counter-icon">
-									<i className="flaticon-coverage"></i>
-								</div>
-								<div className="funfact-counter-number">
-									<h3 className="counter-number">3.3</h3>
-									<span className="funfact-counter-number-postfix">k</span>
-								</div>
-							</div>
-							<p className="funfact-text">Square kilometres<br /> Region Covers</p>
-						</div>
-					</div>
-					<div className="col-xl-3 col-md-6">
-						<div className="funfact-counter-item">
-							<div className="funfact-counter-box">
-								<div className="funfact-counter-icon">
-									<i className="flaticon-landscape"></i>
-								</div>
-								<div className="funfact-counter-number">
-									<h3 className="counter-number">26</h3>
-									<span className="funfact-counter-number-postfix">%</span>
-								</div>
-							</div>
-							<p className="funfact-text">Private & Domestic <br />Garden Land</p>
-						</div>
-					</div>
-					<div className="col-xl-3 col-md-6">
-						<div className="funfact-counter-item">
-							<div className="funfact-counter-box">
-								<div className="funfact-counter-icon">
-									<i className="flaticon-barn-3"></i>
-								</div>
-								<div className="funfact-counter-number">
-									<h3 className="counter-number">4</h3>
-									<span className="funfact-counter-number-postfix">th</span>
-								</div>
-							</div>
-							<p className="funfact-text">Average Costs of Home <br /> Ownership</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
+					<div className="col-lg-3 col-md-3 pb-3">
+						<div className="sm-card">
+							<img src="assets/image/sm.svg" className="img-fluid " alt="img-97" />
+
+						</div >
+					</div >
+					<div className="col-lg-3 col-md-3 pb-3">
+						<div className="sm-card">
+							<img src="assets/image/berita2.jpeg" className="img-fluid " alt="img-97" />
+
+						</div >
+					</div >
+					<div className="col-lg-3 col-md-3 pb-3">
+						<div className="sm-card">
+							<img src="assets/image/berita2.jpeg" className="img-fluid " alt="img-97" />
+
+						</div >
+					</div >
+					<div className="col-lg-3 col-md-3 pb-3">
+						<div className="sm-card">
+							<img src="assets/image/berita2.jpeg" className="img-fluid " alt="img-97" />
+
+						</div >
+					</div >
+
+				</div >
+			</div >
+		</section >
 	)
 }
 export default Section11
