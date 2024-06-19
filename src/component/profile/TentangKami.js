efb3837e503e96f52edfce1417b2d26b15240e12
rizskydarmawann <70370415+rizskydarmawann@users.noreply.github.com>
2024-06-19 14:22:11 +0700

    sc
    sc

--- src/component/profile/TentangKami.js
+++ src/component/profile/TentangKami.js
@@ -2,51 +2,71 @@ import React from "react";
 const TentangKami = () => {
     return (
         <>
-        <div className="page-wrapper">
-            
-            <section className="page-banner">
-                <div className="container">
-                    <div className="page-banner-title">
-                        <h3>Tentang Kami</h3>
-                    </div>
-                </div>
-            </section>
-            <section className="about-one-section">
-                <div className="container">
-                    <div className="row row-gutter-y-40">
-                        <div className="col-lg-12 col-xl-6">
-                            <div className="about-one-inner">
-                                <div className="section-tagline">
-                                    Our introductions
-                                </div>
-                                <h2 className="section-title">Welcome to Mexican City Municipal Council</h2>
-                                <p>Aliquam viverra arcu. Donec aliquet blandit enim feugiat. Suspendisse id quam sed eros tincidunt luctus sit amet eu nibh egestas tempus turpis, sit amet mattis magna varius non.</p>
-                                <h5 className="about-one-inner-text">Denounce with righteous indignation and dislike men who are so beguiled & demoralized our power.</h5>
-                                <div className="row row-gutter-y-30">
-                                    <div className="col-xl-6 col-lg-6 col-md-6">
-                                        <div className="about-one-card">
-                                            <div className="about-one-card-number">01</div>
-                                            <div className="about-one-card-content"><h5>Going Above and Beyond</h5></div>
-                                        </div>
-                                    </div>
-                                    <div className="col-xl-6 col-lg-6 col-md-6">
-                                        <div className="about-one-card">
-                                            <div className="about-one-card-number">02</div>
-                                            <div className="about-one-card-content"><h5>Committed to People First</h5></div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-12 col-xl-6">
-                            <div className="about-one-image">
-                                <img src="assets/image/shapes/shape-1.png" className="floated-image-one" alt="img-58" />
-                                <img src="assets/image/gallery/about-7.jpg" alt="img-59" className="img-fluid" />
-                            </div>
+            <div className="page-wrapper">
+
+                <section className="page-banner">
+                    <div className="container">
+                        <div className="page-banner-title">
+                            <h3>Tentang Kami</h3>
                         </div>
                     </div>
-                </div>
-            </section>
+                </section>
+                <section className="about-one-section">
+                    <div className="container">
+                        <div className="row row-gutter-y-40 border">
+                            <div className="col-lg-12 col-xl-6">
+                                <div className="about-one-image">
+                                    <img src="assets/image/logo-tk.svg" alt="img-59" className="img-fluid" />
+                                </div>
+                            </div>
+                            <div className="col-lg-12 col-xl-6">
+                                <div className="about-one-inner">
+
+                                    <h2 className="section-title">Tentang Kami</h2>
+                                    <p className="text-justify">
+                                        Komite Nasional Ekonomi dan Keuangan Syariah (KNEKS) merupakan perubahan dari KNKS untuk peningkatan pembangunan ekosistem ekonomi dan keuangan syariah serta menjadikan Indonesia sebagai Pusat Halal Dunia.<br />
+
+                                        Pencanangan titik awal untuk memposisikan Indonesia sebagai salah satu pelaku utama dan hub ekonomi syariah dunia dilakukan seiring dengan peluncuran Masterplan Ekonomi Syariah Indonesia pada bulan Mei 2019.
+                                    </p>
+
+                                </div>
+                            </div>
+                            <div className="col-lg-12 col-xl-6">
+                                <div className="about-one-inner">
+
+                                    <h2 className="section-title">Sejarah KNEKS</h2>
+                                    <p className="text-justify">
+                                    Dalam rangka mendukung pembangunan ekonomi nasional dan mendorong percepatan pengembangan sektor keuangan syariah, pemerintah secara khusus mendirikan KNKS pada tanggal 8 November 2016 agar dapat meningkatkan efektifitas, efisiensi pelaksanaan rencana pembangunan nasional bidang keuangan dan ekonomi Syariah. Selanjutnya sejak diundangkan tanggal 10 Februari 2020, pemerintah melakukan perubahan Komite Nasional Keuangan Syariah menjadi Komite Nasional Ekonomi dan Keuangan Syariah yang bertujuan meningkatkan pembangunan ekosistem ekonomi dan keuangan syariah guna mendukung pembangunan ekonomi nasional.
+                                    </p>
+
+                                </div>
+                            </div>
+                            <div className="col-lg-12 col-xl-6">
+                                <div className="about-one-image">
+                                    <img src="assets/image/poto-sj.svg" alt="img-59" className="img-fluid" />
+                                </div>
+                            </div>
+                            <div className="col-lg-12 col-xl-6">
+                                <div className="about-one-image">
+                                    <img src="assets/image/poto-hk.svg" alt="img-59" className="img-fluid" />
+                                </div>
+                            </div>
+                            <div className="col-lg-12 col-xl-6">
+                                <div className="about-one-inner ">
+
+                                    <h2 className="section-title text-center">Landasan Hukum</h2>
+                                    <p className="text-center">Peraturan Presiden RI No. 28 tahun 2020</p>
+                                    <p className="text-justify">
+                                    Komite Nasional Ekonomi dan Keuangan Syariah. JDIH Marves – Dalam rangka peningkatan pembangunan ekosistem ekonomi dan keuangan syariah guna mendukung pembangunan ekonomi nasional, perlu dilakukan perubahan Komite Nasionai Keuangan Syariah menjadi Komite Nasional Ekonomi dan Keuangan Syariah.
+                                    </p>
+
+                                </div>
+                            </div>
+                            
+                        </div>
+                        
+                    </div>
+                </section>
             </div>
         </>
     )
