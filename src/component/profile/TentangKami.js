a003103a2bb0962bac46100d1f77941f39b035cf
rizskydarmawann <70370415+rizskydarmawann@users.noreply.github.com>
2024-06-20 14:15:54 +0700

    tentamg kami
    update

--- src/component/profile/TentangKami.js
+++ src/component/profile/TentangKami.js
@@ -78,19 +78,64 @@ const TentangKami = () => {
                                     Huruf N
                                 </h5>
                                 <p className="text-justify">
-                                Huruf N dari KNEKS ini dimodifikasi sedemikian rupa menjadi unique point dari logo. Penempatannya yang sejajar dengan dan menjadi kesatuan dalam huruf KNEKS dimaksudkan agar logo terlihat lebih sederhana. Agar perhatian atau point of view audience langsung tertuju pada KNEKS. Sehingga hal ini membuat audience mengenali logo KNEKS dengan tepat dan seketika. Huruf N ini kemudian dimodIfikasi menjadi garis bersudut dengan tiga titik, yang menggambarkan tiga tugas utama KNEKS, yaitu mempercepat, memperluas dan memajukan pengembangan ekonomi dan keuangan syariah dalam rangka memperkuat ketahanan ekonomi nasional.
+                                    Huruf N dari KNEKS ini dimodifikasi sedemikian rupa menjadi unique point dari logo. Penempatannya yang sejajar dengan dan menjadi kesatuan dalam huruf KNEKS dimaksudkan agar logo terlihat lebih sederhana. Agar perhatian atau point of view audience langsung tertuju pada KNEKS. Sehingga hal ini membuat audience mengenali logo KNEKS dengan tepat dan seketika. Huruf N ini kemudian dimodIfikasi menjadi garis bersudut dengan tiga titik, yang menggambarkan tiga tugas utama KNEKS, yaitu mempercepat, memperluas dan memajukan pengembangan ekonomi dan keuangan syariah dalam rangka memperkuat ketahanan ekonomi nasional.
 
                                 </p>
 
                             </div>
                             <div className="col-lg-12 col-xl-12">
                                 <h5 class="about-title">
-                                Pintu Gerbang
+                                    Pintu Gerbang
                                 </h5>
                                 <p className="text-justify">
-                                Sengaja dirancang agar menyerupai pintu gerbang yang berarti keterbukaan. KNEKS terbuka terhadap pemangku kepentingan untuk pengembangan ekonomi dan keuangan syariah. Pintu gerbang ini juga melambangkan keterbukaan terhadap investor asing untuk menanamkan modalnya di Indonesia.
+                                    Sengaja dirancang agar menyerupai pintu gerbang yang berarti keterbukaan. KNEKS terbuka terhadap pemangku kepentingan untuk pengembangan ekonomi dan keuangan syariah. Pintu gerbang ini juga melambangkan keterbukaan terhadap investor asing untuk menanamkan modalnya di Indonesia.
                                 </p>
 
+                            </div>
+                            <div className="col-lg-12 col-xl-12">
+                                <h5 class="about-title">
+                                    Arah
+                                </h5>
+                                <p className="text-justify">
+                                    Arah panah ke samping kanan atas adalah simbol dari progresif. Arah panah ke samping kanan atas ini mampu memberikan kesan positif. Melalui simbol ini ingin dikomunikasikan fungsi-fungsi KNEKS yang progresif, seperti pemberian rekomendasi arah kebijakan dan program strategis pembangunan nasional, dan lain-lain.
+                                </p>
+
+                            </div>
+                            <div className="col-lg-12 col-xl-12">
+                                <div className="about-one-inner text-center">
+                                    <h2 className="section-title text-center">Tugas KNEKS</h2>
+                                    <p className="text-justify">
+                                        KNEKS bertugas mempercepat, memperluas dan memajukan pengembangan ekonomi dan keuangan syariah dalam rangka mendukung ketahanan ekonomi nasional
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="col-lg-12 col-xl-12">
+                                <div className="about-one-inner ">
+                                    <h2 className="section-title text-center">Fungsi KNEKS</h2>
+                                    
+                                    <ol  className="text-justify">
+                                        <li>
+                                            <p>
+                                            Pemberian rekomendasi arah kebijakan dan program strategis pembangunan nasional di sektor ekonomi dan keuangan syariah.
+                                        </p>
+                                        </li>
+                                        <li>
+                                        <p>
+                                        Pelaksanaan koordinasi, sinkronisasi, sinergisitas penyusunan dan pelaksanaan rencana araha kebijakan dan program strategis pada sektor ekonomi dan keuangan syariah.
+                                        </p>
+                                        </li>
+                                        <li>
+                                            <p>
+                                        Perumusan dan pemberian rekomendasi atas penyelesaian masalah di sektor ekonomi dan keuangan syariah.
+                                        </p>
+                                        </li>
+                                        <li>
+                                            <p>
+                                            Pemantauan dan evaluasi atas pelaksanaan arah kebijakan dan program strategis di sektor ekonomi dan keuangan syariah.
+                                            </p>
+                                        </li>
+                                    </ol>
+                                </div>
                             </div>
 
                         </div>
