da8979737e199677a223f9334c9fc88f6d7401dd
Rizsky Darmawan <darmawanrizsky@gmail.com>
2024-07-09 05:54:15 +0700

    sc
    sc

--- src/component/berita/SiaranPersDetail.js
+++ src/component/berita/SiaranPersDetail.js
@@ -39,8 +39,13 @@ const SiaranPersDetail = () => {
                     <div className="container">
                         <div className="row">
                             <div className="col-lg-12">
+                                <div className="event-details-content-box">
+                                    <h4>{item.title}</h4>
+                                </div>
+                            </div>
+                            <div className="col-lg-12">
                                 <div className="event-details-inner-box">
-                                    <img src={`${process.env.PUBLIC_URL}/${item.foto}`} className="img-fluid" alt={item.title} />
+                                    <img src={`${process.env.PUBLIC_URL}/${item.foto}`} width={`100%`} className="img-fluid" alt={item.title} />
                                     <div className="event-details-meta">
                                         <div className="event-details-meta-number">
                                             <span>{arrTgl[0]}</span>
@@ -51,9 +56,22 @@ const SiaranPersDetail = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-lg-8">
-                                <div class="event-details-content-box">
-                                    <h4>{item.title}</h4>
+                            <div className="row pb-5">
+                                <div className="col-lg-3">
+                                    <img src={`${process.env.PUBLIC_URL}/${item.foto}`} width={`100%`} className="img-fluid" alt={item.title} />
+                                </div>
+                                <div className="col-lg-3">
+                                    <img src={`${process.env.PUBLIC_URL}/${item.foto}`} width={`100%`} className="img-fluid" alt={item.title} />
+                                </div>
+                                <div className="col-lg-3">
+                                    <img src={`${process.env.PUBLIC_URL}/${item.foto}`} width={`100%`} className="img-fluid" alt={item.title} />
+                                </div>
+                                <div className="col-lg-3">
+                                    <img src={`${process.env.PUBLIC_URL}/${item.foto}`} width={`100%`} className="img-fluid" alt={item.title} />
+                                </div>
+                            </div>
+                            <div className="col-lg-12">
+                                <div className="event-details-content-box">
                                     <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
                                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum ley of type and scrambled it to make a type specimen book.</p>
                                 </div>
