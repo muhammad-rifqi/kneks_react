bb0d7264a0ae27ebccbe38c3dbd2ee1d081cabfc
Rizsky Darmawan <darmawanrizsky@gmail.com>
2024-07-27 06:35:28 +0700

    up
    up

--- src/component/profile/TentangKami.js
+++ src/component/profile/TentangKami.js
@@ -1,6 +1,8 @@
 import React from "react";
 const TentangKami = () => {
+
     return (
+
         <>
             <div className="page-wrapper">
                 <section className="page-banner">
@@ -24,65 +26,13 @@ const TentangKami = () => {
 
                             {/* konten sebelah kanan */}
                             <div className="col-lg-12 col-xl-6">
-                                <div className="about-one-inner">
+                                <div className="about-one-inner-x">
                                     <h2 className="section-title">Tentang Kami</h2>
                                     <p>Komite Nasional Ekonomi dan Keuangan Syariah (KNEKS) merupakan perubahan dari KNKS untuk peningkatan pembangunan ekosistem ekonomi dan keuangan syariah serta menjadikan Indonesia sebagai Pusat Halal Dunia.</p>
                                     <p>Pencanangan titik awal untuk memposisikan Indonesia sebagai salah satu pelaku utama dan hub ekonomi syariah dunia dilakukan seiring dengan peluncuran Masterplan Ekonomi Syariah Indonesia pada bulan Mei 2019.</p>
                                 </div>
-                                <div className="col-lg-12 col-xl-12">
-                                    <h5 class="about-title">
-                                        Pintu Gerbang
-                                    </h5>
-                                    <p className="text-justify">
-                                        Sengaja dirancang agar menyerupai pintu gerbang yang berarti keterbukaan. KNEKS terbuka terhadap pemangku kepentingan untuk pengembangan ekonomi dan keuangan syariah. Pintu gerbang ini juga melambangkan keterbukaan terhadap investor asing untuk menanamkan modalnya di Indonesia.
-                                    </p>
 
-                                </div>
-                                <div className="col-lg-12 col-xl-12">
-                                    <h5 class="about-title">
-                                        Arah
-                                    </h5>
-                                    <p className="text-justify">
-                                        Arah panah ke samping kanan atas adalah simbol dari progresif. Arah panah ke samping kanan atas ini mampu memberikan kesan positif. Melalui simbol ini ingin dikomunikasikan fungsi-fungsi KNEKS yang progresif, seperti pemberian rekomendasi arah kebijakan dan program strategis pembangunan nasional, dan lain-lain.
-                                    </p>
 
-                                </div>
-                                <div className="col-lg-12 col-xl-12">
-                                    <div className="about-one-inner text-center">
-                                        <h2 className="section-title text-center">Tugas KNEKS</h2>
-                                        <p className="text-justify">
-                                            KNEKS bertugas mempercepat, memperluas dan memajukan pengembangan ekonomi dan keuangan syariah dalam rangka mendukung ketahanan ekonomi nasional
-                                        </p>
-                                    </div>
-                                </div>
-                                <div className="col-lg-12 col-xl-12">
-                                    <div className="about-one-inner ">
-                                        <h2 className="section-title text-center">Fungsi KNEKS</h2>
-
-                                        <ol className="text-justify">
-                                            <li>
-                                                <p>
-                                                    Pemberian rekomendasi arah kebijakan dan program strategis pembangunan nasional di sektor ekonomi dan keuangan syariah.
-                                                </p>
-                                            </li>
-                                            <li>
-                                                <p>
-                                                    Pelaksanaan koordinasi, sinkronisasi, sinergisitas penyusunan dan pelaksanaan rencana araha kebijakan dan program strategis pada sektor ekonomi dan keuangan syariah.
-                                                </p>
-                                            </li>
-                                            <li>
-                                                <p>
-                                                    Perumusan dan pemberian rekomendasi atas penyelesaian masalah di sektor ekonomi dan keuangan syariah.
-                                                </p>
-                                            </li>
-                                            <li>
-                                                <p>
-                                                    Pemantauan dan evaluasi atas pelaksanaan arah kebijakan dan program strategis di sektor ekonomi dan keuangan syariah.
-                                                </p>
-                                            </li>
-                                        </ol>
-                                    </div>
-                                </div>
 
                             </div>
 
@@ -96,7 +46,7 @@ const TentangKami = () => {
 
                             {/* konten sejarah deskripsi */}
                             <div className="col-lg-12 col-xl-6">
-                                <div className="about-one-inner">
+                                <div className="about-one-inner-x">
                                     <h2 className="section-title">Sejarah KNEKS</h2>
                                     <p>Dalam rangka mendukung pembangunan ekonomi nasional dan mendorong percepatan pengembangan sektor keuangan syariah, pemerintah secara khusus mendirikan KNKS pada tanggal 8 November 2016 agar dapat meningkatkan efektifitas, efisiensi pelaksanaan rencana pembangunan nasional bidang keuangan dan ekonomi Syariah. Selanjutnya sejak diundangkan tanggal 10 Februari 2020, pemerintah melakukan perubahan Komite Nasional Keuangan Syariah menjadi Komite Nasional Ekonomi dan Keuangan Syariah yang bertujuan meningkatkan pembangunan ekosistem ekonomi dan keuangan syariah guna mendukung pembangunan ekonomi nasional.</p>
                                 </div>
@@ -105,7 +55,7 @@ const TentangKami = () => {
                             {/* konten sejarah images */}
                             <div className="col-lg-12 col-xl-6">
                                 <div className="about-one-image">
-                                    <img src="assets/image/gallery/grahadi.jpeg" alt="img-59" className="img-fluid" />
+                                    <img src="assets/image/sejarah.svg" alt="sejarah" className="img-fluid" />
                                 </div>
                             </div>
 
@@ -120,13 +70,13 @@ const TentangKami = () => {
                             {/* gambar landasan hukum */}
                             <div className="col-lg-12 col-xl-6">
                                 <div className="about-one-image">
-                                    <img src="assets/image/gallery/grahadi.jpeg" alt="img-59" className="img-fluid" />
+                                    <img src="assets/image/hukum.svg" alt="hukum" className="img-fluid" />
                                 </div>
                             </div>
 
                             {/* deskripsi landasan hukum */}
                             <div className="col-lg-12 col-xl-6">
-                                <div className="about-one-inner">
+                                <div className="about-one-inner-x">
                                     <h2 className="section-title" style={{ textAlign: 'center' }}>Landasan Hukum</h2>
                                     <p className="section-title" style={{ textAlign: 'center', fontSize: '12pt', }}>Peraturan Presiden RI No.28 Tahun 2020</p>
                                     <p>Komite Nasional Ekonomi dan Keuangan Syariah. JDIH Marves – Dalam rangka peningkatan pembangunan ekosistem ekonomi dan keuangan syariah guna mendukung pembangunan ekonomi nasional, perlu dilakukan perubahan Komite Nasionai Keuangan Syariah menjadi Komite Nasional Ekonomi dan Keuangan Syariah.</p>
@@ -147,7 +97,7 @@ const TentangKami = () => {
 
                             {/* konten sebelah kanan */}
                             <div className="col-lg-12 col-xl-12">
-                                <div className="about-one-inner">
+                                <div className="about-one-inner-x">
                                     <h2 className="section-title" style={{ textAlign: 'center' }}>Filosofi Logo</h2>
                                 </div>
                             </div>
@@ -155,7 +105,7 @@ const TentangKami = () => {
                             {/* konten sebelah kiri */}
                             <div className="col-lg-12 col-xl-12">
                                 <div className="about-one-image" style={{ textAlign: 'center' }}>
-                                    <img src="assets/image/gallery/img-knks-1.png" alt="img-59" className="img-fluid" />
+                                    <img src="assets/image/filosofiTk.svg" alt="img-59" className="img-fluid" />
                                 </div>
                             </div>
 
@@ -167,21 +117,21 @@ const TentangKami = () => {
 
                             {/* konten sebelah kanan */}
                             <div className="col-lg-12 col-xl-12">
-                                <div className="about-one-inner">
+                                <div className="about-one-inner-x">
                                     <h2 className="section-title">Huruf N</h2>
                                     <p>Huruf N dari KNEKS ini dimodifikasi sedemikian rupa menjadi unique point dari logo. Penempatannya yang sejajar dengan dan menjadi kesatuan dalam huruf KNEKS dimaksudkan agar logo terlihat lebih sederhana. Agar perhatian atau point of view audience langsung tertuju pada KNEKS. Sehingga hal ini membuat audience mengenali logo KNEKS dengan tepat dan seketika. Huruf N ini kemudian dimodIfikasi menjadi garis bersudut dengan tiga titik, yang menggambarkan tiga tugas utama KNEKS, yaitu mempercepat, memperluas dan memajukan pengembangan ekonomi dan keuangan syariah dalam rangka memperkuat ketahanan ekonomi nasional.</p>
                                 </div>
                             </div>
 
                             <div className="col-lg-12 col-xl-12">
-                                <div className="about-one-inner">
+                                <div className="about-one-inner-x">
                                     <h2 className="section-title">Pintu Gerbang</h2>
                                     <p>Sengaja dirancang agar menyerupai pintu gerbang yang berarti keterbukaan. KNEKS terbuka terhadap pemangku kepentingan untuk pengembangan ekonomi dan keuangan syariah. Pintu gerbang ini juga melambangkan keterbukaan terhadap investor asing untuk menanamkan modalnya di Indonesia.</p>
                                 </div>
                             </div>
 
                             <div className="col-lg-12 col-xl-12">
-                                <div className="about-one-inner">
+                                <div className="about-one-inner-x">
                                     <h2 className="section-title">Arah</h2>
                                     <p>Arah panah ke samping kanan atas adalah simbol dari progresif. Arah panah ke samping kanan atas ini mampu memberikan kesan positif. Melalui simbol ini ingin dikomunikasikan fungsi-fungsi KNEKS yang progresif, seperti pemberian rekomendasi arah kebijakan dan program strategis pembangunan nasional, dan lain-lain.</p>
                                 </div>
@@ -197,7 +147,7 @@ const TentangKami = () => {
 
                             {/* deskripsi landasan hukum */}
                             <div className="col-lg-12 col-xl-12">
-                                <div className="about-one-inner">
+                                <div className="about-one-inner-x">
                                     <h2 className="section-title" style={{ textAlign: 'center' }}>Tugas KNEKS</h2>
                                     <p>KNEKS bertugas mempercepat, memperluas dan memajukan pengembangan ekonomi dan keuangan syariah dalam rangka mendukung ketahanan ekonomi nasional.</p>
                                 </div>
@@ -205,7 +155,7 @@ const TentangKami = () => {
 
                             {/* deskripsi landasan hukum */}
                             <div className="col-lg-12 col-xl-12">
-                                <div className="about-one-inner">
+                                <div className="about-one-inner-x">
                                     <h2 className="section-title" style={{ textAlign: 'center' }}>Fungsi KNEKS</h2>
                                     <ol>
                                         <li>Pemberian rekomendasi arah kebijakan dan program strategis pembangunan nasional di sektor ekonomi dan keuangan syariah.</li>
