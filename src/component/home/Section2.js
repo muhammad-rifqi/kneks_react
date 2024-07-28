1ec82f85d469ef8dc0e843e10048f260435d6d3c
Rizsky Darmawan <darmawanrizsky@gmail.com>
2024-07-28 21:58:42 +0700

    up
    up

--- src/component/home/Section2.js
+++ src/component/home/Section2.js
@@ -1,14 +1,14 @@
 import React from "react";
 const Section2 = () => {
     return (
-        <section className="department-section">
+        <section className="department-section-x">
             <div className="container">
-                <div className="department-section-inner">
-                    <div className="row justify-content-center row-gutter-y-40">
-                        <div className="col-xl-2 col-lg-4 col-md-6">
-                            <div className="department-card">
-                                <div className="department-card-icon">
-                                    {/* <a href="javascript:void(0)"><i className="flaticon-lotus"></i></a> */}
+
+                <div className="row justify-content-center">
+                    <div className="col-lg-2 col-md-2">
+                        <div className="card">
+                            <div className="card-body text-center">
+                                <div className="department-card-gambar">
                                     <a href="/industri-produk-halal"><img src="/assets/image/halal.svg" alt="Industri Produksi Halal" className="img-b img-fluid" /></a>
                                 </div>
                                 <div className="department-card-content">
@@ -16,53 +16,60 @@ const Section2 = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="col-xl-2 col-lg-4 col-md-6">
-                            <div className="department-card">
-                                <div className="department-card-icon">
-                                    <a href="/jasa-keuangan-syariah"><i className="flaticon-balance-1"></i></a>
+                    </div>
+
+                    <div className="col-lg-2 col-md-2">
+                        <div className="card">
+                            <div className="card-body text-center">
+                                <div className="department-card-gambar">
+                                    <a href="/jasa-keuangan-syariah"><img src="/assets/image/jasa.svg" alt="Jasa Keuangan Syariah" className="img-b img-fluid" /></a>
                                 </div>
                                 <div className="department-card-content">
                                     <h5><a href="/jasa-keuangan-syariah">Jasa Keuangan Syariah</a></h5>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-xl-2 col-lg-4 col-md-6">
-                            <div className="department-card">
-                                <div className="department-card-icon">
-                                    <a href="/keuangan-sosial-syariah"><i className="flaticon-parthenon"></i></a>
+                    </div>
+                    <div className="col-lg-2 col-md-2">
+                        <div className="card">
+                            <div className="card-body text-center">
+                                <div className="department-card-gambar">
+                                    <a href="/keuangan-sosial-syariah"><img src="/assets/image/sosial.svg" alt="Keuangan Sosial Syariah" className="img-b img-fluid" /></a>
                                 </div>
                                 <div className="department-card-content">
                                     <h5><a href="/keuangan-sosial-syariah">Keuangan Sosial Syariah</a></h5>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-xl-2 col-lg-4 col-md-6">
-                            <div className="department-card">
-                                <div className="department-card-icon">
-                                    <a href="/bisnis-dan-kewiraushaan-syariah"><i className="flaticon-suitcase"></i></a>
+                    </div>
+                    <div className="col-lg-2 col-md-2">
+                        <div className="card">
+                            <div className="card-body text-center">
+                                <div className="department-card-gambar">
+                                    <a href="/bisnis-dan-kewiraushaan-syariah"><img src="/assets/image/sme.svg" alt="Bisnis Dan Kewirausahaan Syariah" className="img-b img-fluid" /></a>
                                 </div>
                                 <div className="department-card-content">
                                     <h5><a href="/bisnis-dan-kewiraushaan-syariah">Bisnis Dan Kewirausahaan Syariah</a></h5>
                                 </div>
                             </div>
                         </div>
-
-                        <div className="col-xl-2 col-lg-4 col-md-6">
-                            <div className="department-card">
-                                <div className="department-card-icon">
-                                    <a href="/infrastruktur-ekosistem-syariah"><i className="flaticon-industry"></i></a>
+                    </div>
+                    <div className="col-lg-2 col-md-2">
+                        <div className="card">
+                            <div className="card-body text-center">
+                                <div className="department-card-gambar">
+                                    <a href="/infrastruktur-ekosistem-syariah"><img src="/assets/image/infra.svg" alt="Bisnis Dan Kewirausahaan Syariah" className="img-b img-fluid" /></a>
                                 </div>
                                 <div className="department-card-content">
                                     <h5><a href="/infrastruktur-ekosistem-syariah">Infrastruktur Ekosistem Syariah </a></h5>
                                 </div>
                             </div>
                         </div>
-
-
-
                     </div>
                 </div>
             </div>
+
+
         </section>
     )
 }
--- src/component/home/Section3.js
+++ src/component/home/Section3.js
@@ -25,7 +25,7 @@ const Section3 = () => {
                     <div className="col-lg-6">
                         <div className="about-inner">
                             <div className="section-title-box">
-                                <div className="section-tagline">About Us</div>
+                                <div className="section-tagline">Perkenalkan</div>
                                 <h2 className="section-title">Komite Nasional Ekonomi dan Keuangan Syariah (KNEKS)</h2>
                                 <p>merupakan perubahan dari KNKS untuk peningkatan pembangunan ekosistem ekonomi dan keuangan syariah serta menjadikan Indonesia sebagai Pusat Halal Dunia.  Pencanangan titik awal untuk memposisikan Indonesia sebagai salah satu pelaku utama dan hub ekonomi syariah dunia dilakukan seiring dengan peluncuran Masterplan Ekonomi Syariah Indonesia pada bulan Mei 2019.</p>
                             </div>
