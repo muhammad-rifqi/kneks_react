bd6d07b633291fac107b1307886d32c4985b4d4b
rizskydarmawann <70370415+rizskydarmawann@users.noreply.github.com>
2024-07-31 11:55:58 +0700

    up
    up

--- src/component/home/Section6.js
+++ src/component/home/Section6.js
@@ -5,20 +5,16 @@ const Section6 = () => {
 			<div className="container">
 				<div className="blog-box">
 					<div className="section-title-box text-center">
-						{/* <div className="section-tagline">News</div> */}
 						<h2 className="section-title">Rekomendasi Agenda Lainnya</h2>
 					</div>
 				</div>
 				<div className="row ">
 					<div className="col-lg-4 col-md-6">
-						<div className="card">
+						<div className="card shadow p-3 mb-5 rounded" style={{maxWidth: `18rem`, background:`#146AA4`, color:`#ffffff`}}>
+							<div className="card-header" style={{borderBottom: `1px solid #ffffff`, fontSize:`18px`, paddingTop:`10px`,background:`#146AA4`}}>Webinar Keuangan</div>
 							<div className="card-body">
-								<div className="avatar-text rounded-2 mb-4">
-									<i className="fa fa-calendar"></i>
-								</div>
-								<h6 className="fw-bold mb-3 text-truncate-1-line">Website design and development</h6>
-								<p className="text-muted mb-4 text-truncate-3-line">Website design and development - designing and building a website to meet specific requirements, such as the look and feel, functionality, and content.</p>
-								<a href="javascript:void(0);" className="d-block fs-10 fw-bold text-dark text-uppercase text-spacing-1">Learn More →</a>
+								<p className="card-text">24/05/2024 - 24/05/2024</p>
+								<p className="card-text">12:00 - 14:00 WIP</p>
 							</div>
 						</div>
 					</div>
