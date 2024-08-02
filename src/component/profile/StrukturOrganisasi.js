bc0cb8e94bcec57907d487cb18f72a4337e0c01e
rizskydarmawann <70370415+rizskydarmawann@users.noreply.github.com>
2024-08-02 11:34:54 +0700

    up
    up

--- src/component/profile/StrukturOrganisasi.js
+++ src/component/profile/StrukturOrganisasi.js
@@ -1,93 +1,161 @@
 import React from "react";
 const StrukturOrganisasi = () => {
-    return (
-        <>
-            <div className="page-wrapper">
+	return (
+		<>
+			<div className="page-wrapper">
 
-                <section className="page-banner">
-                    <div className="container">
-                        <div className="page-banner-title">
-                            <h3>Struktur Organisasi</h3>
-                        </div>
-                    </div>
-                </section>
-                <section className="about-one-section">
-                    <div className="container">
-                        <div className="row row-gutter-y-40 text-center">
-                            <div className="col-lg-12 col-xl-12">
-                                <div className="about-one-image">
-                                    <img src="assets/image/struktur1.svg" alt="img-59" className="img-fluid" />
-                                </div>
-                            </div>
-
-                        </div>
-                        <div className="row row-gutter-y-30">
-					<div className="col-12 col-md-6 col-xl-3">
-						<div className="team-card">
-							<div className="team-card-img">
-								<img src="assets/image/team/team-1.jpg" className="img-fluid" alt="img-40"/>
-								<div className="team-card-icon">
-									<a href="#" className="pinterest"><i className="fa-brands fa-pinterest-p"></i></a>
-									<a href="#" className="twitter"><i className="fa-brands fa-twitter"></i></a>
-									<a href="#" className="facebook"><i className="fa-brands fa-facebook"></i></a>
-								</div>
-							</div>
-							<div className="team-card-content">
-								<h4><a href="team-details.html">Sarah Albert</a></h4>
-								<p>Consultant</p>
-							</div>
+				<section className="page-banner">
+					<div className="container">
+						<div className="page-banner-title">
+							<h3>Struktur Organisasi</h3>
 						</div>
 					</div>
-					<div className="col-12 col-md-6 col-xl-3">
-						<div className="team-card">
-							<div className="team-card-img">
-								<img src="assets/image/team/team-2.jpg" className="img-fluid" alt="img-41" />
-								<div className="team-card-icon">
-									<a href="#" className="pinterest"><i className="fa-brands fa-pinterest-p"></i></a>
-									<a href="#" className="twitter"><i className="fa-brands fa-twitter"></i></a>
-									<a href="#" className="facebook"><i className="fa-brands fa-facebook"></i></a>
+				</section>
+				<section className="about-one-section">
+					<div className="container">
+						<div className="row row-gutter-y-40 text-center">
+							<div className="col-lg-12 col-xl-12">
+								<div className="about-one-image">
+									<img src="assets/image/struktur1.svg" alt="img-59" className="img-fluid" />
 								</div>
 							</div>
-							<div className="team-card-content">
-								<h4><a href="team-details.html">Mike Hardson</a></h4>
-								<p>Consultant</p>
-							</div>
 						</div>
-					</div>
-					<div className="col-12 col-md-6 col-xl-3">
-						<div className="team-card">
-							<div className="team-card-img">
-								<img src="assets/image/team/team-3.jpg" className="img-fluid" alt="img-42"/>
-								<div className="team-card-icon">
-									<a href="#" className="pinterest"><i className="fa-brands fa-pinterest-p"></i></a>
-									<a href="#" className="twitter"><i className="fa-brands fa-twitter"></i></a>
-									<a href="#" className="facebook"><i className="fa-brands fa-facebook"></i></a>
+						<div className="row row-gutter-y-30">
+							<div className="blog-box-manajemen">
+								<div className="section-title-box text-center">
+									<h2 className="section-title">Manajemen Eksekutif</h2>
 								</div>
 							</div>
-							<div className="team-card-content">
-								<h4><a href="team-details.html">Christine Eve</a></h4>
-								<p>Consultant</p>
-							</div>
-						</div>
-					</div>
-					<div className="col-12 col-md-6 col-xl-3">
-						<div className="team-card">
-							<div className="team-card-img">
-								<img src="assets/image/team/team-4.jpg" className="img-fluid" alt="img-43"/>
-								<div className="team-card-icon">
-									<a href="#" className="pinterest"><i className="fa-brands fa-pinterest-p"></i></a>
-									<a href="#" className="twitter"><i className="fa-brands fa-twitter"></i></a>
-									<a href="#" className="facebook"><i className="fa-brands fa-facebook"></i></a>
+							<div className="col-12 col-md-6 col-xl-3">
+								<div className="team-card">
+									<div className="team-card-img">
+										<img src="assets/image/team/team-1.jpg" className="img-fluid" alt="img-40" />
+										<div className="team-card-icon">
+											<a href="#" className="pinterest"><i className="fa-brands fa-pinterest-p"></i></a>
+											<a href="#" className="twitter"><i className="fa-brands fa-twitter"></i></a>
+											<a href="#" className="facebook"><i className="fa-brands fa-facebook"></i></a>
+										</div>
+									</div>
+									<div className="team-card-content">
+										<h4><a href="team-details.html">Sarah Albert</a></h4>
+										<p>Consultant</p>
+									</div>
 								</div>
 							</div>
-							<div className="team-card-content">
-								<h4><a href="team-details.html">John Martin</a></h4>
-								<p>Consultant</p>
+							<div className="col-12 col-md-6 col-xl-3">
+								<div className="team-card">
+									<div className="team-card-img">
+										<img src="assets/image/team/team-2.jpg" className="img-fluid" alt="img-41" />
+										<div className="team-card-icon">
+											<a href="#" className="pinterest"><i className="fa-brands fa-pinterest-p"></i></a>
+											<a href="#" className="twitter"><i className="fa-brands fa-twitter"></i></a>
+											<a href="#" className="facebook"><i className="fa-brands fa-facebook"></i></a>
+										</div>
+									</div>
+									<div className="team-card-content">
+										<h4><a href="team-details.html">Mike Hardson</a></h4>
+										<p>Consultant</p>
+									</div>
+								</div>
+							</div>
+							<div className="col-12 col-md-6 col-xl-3">
+								<div className="team-card">
+									<div className="team-card-img">
+										<img src="assets/image/team/team-3.jpg" className="img-fluid" alt="img-42" />
+										<div className="team-card-icon">
+											<a href="#" className="pinterest"><i className="fa-brands fa-pinterest-p"></i></a>
+											<a href="#" className="twitter"><i className="fa-brands fa-twitter"></i></a>
+											<a href="#" className="facebook"><i className="fa-brands fa-facebook"></i></a>
+										</div>
+									</div>
+									<div className="team-card-content">
+										<h4><a href="team-details.html">Christine Eve</a></h4>
+										<p>Consultant</p>
+									</div>
+								</div>
+							</div>
+							<div className="col-12 col-md-6 col-xl-3">
+								<div className="team-card">
+									<div className="team-card-img">
+										<img src="assets/image/team/team-4.jpg" className="img-fluid" alt="img-43" />
+										<div className="team-card-icon">
+											<a href="#" className="pinterest"><i className="fa-brands fa-pinterest-p"></i></a>
+											<a href="#" className="twitter"><i className="fa-brands fa-twitter"></i></a>
+											<a href="#" className="facebook"><i className="fa-brands fa-facebook"></i></a>
+										</div>
+									</div>
+									<div className="team-card-content">
+										<h4><a href="team-details.html">John Martin</a></h4>
+										<p>Consultant</p>
+									</div>
+								</div>
+							</div>
+							<div className="col-12 col-md-6 col-xl-3">
+								<div className="team-card">
+									<div className="team-card-img">
+										<img src="assets/image/team/team-4.jpg" className="img-fluid" alt="img-43" />
+										<div className="team-card-icon">
+											<a href="#" className="pinterest"><i className="fa-brands fa-pinterest-p"></i></a>
+											<a href="#" className="twitter"><i className="fa-brands fa-twitter"></i></a>
+											<a href="#" className="facebook"><i className="fa-brands fa-facebook"></i></a>
+										</div>
+									</div>
+									<div className="team-card-content">
+										<h4><a href="team-details.html">John Martin</a></h4>
+										<p>Consultant</p>
+									</div>
+								</div>
+							</div>
+							<div className="col-12 col-md-6 col-xl-3">
+								<div className="team-card">
+									<div className="team-card-img">
+										<img src="assets/image/team/team-4.jpg" className="img-fluid" alt="img-43" />
+										<div className="team-card-icon">
+											<a href="#" className="pinterest"><i className="fa-brands fa-pinterest-p"></i></a>
+											<a href="#" className="twitter"><i className="fa-brands fa-twitter"></i></a>
+											<a href="#" className="facebook"><i className="fa-brands fa-facebook"></i></a>
+										</div>
+									</div>
+									<div className="team-card-content">
+										<h4><a href="team-details.html">John Martin</a></h4>
+										<p>Consultant</p>
+									</div>
+								</div>
+							</div>
+							<div className="col-12 col-md-6 col-xl-3">
+								<div className="team-card">
+									<div className="team-card-img">
+										<img src="assets/image/team/team-4.jpg" className="img-fluid" alt="img-43" />
+										<div className="team-card-icon">
+											<a href="#" className="pinterest"><i className="fa-brands fa-pinterest-p"></i></a>
+											<a href="#" className="twitter"><i className="fa-brands fa-twitter"></i></a>
+											<a href="#" className="facebook"><i className="fa-brands fa-facebook"></i></a>
+										</div>
+									</div>
+									<div className="team-card-content">
+										<h4><a href="team-details.html">John Martin</a></h4>
+										<p>Consultant</p>
+									</div>
+								</div>
+							</div>
+							<div className="col-12 col-md-6 col-xl-3">
+								<div className="team-card">
+									<div className="team-card-img">
+										<img src="assets/image/team/team-4.jpg" className="img-fluid" alt="img-43" />
+										<div className="team-card-icon">
+											<a href="#" className="pinterest"><i className="fa-brands fa-pinterest-p"></i></a>
+											<a href="#" className="twitter"><i className="fa-brands fa-twitter"></i></a>
+											<a href="#" className="facebook"><i className="fa-brands fa-facebook"></i></a>
+										</div>
+									</div>
+									<div className="team-card-content">
+										<h4><a href="team-details.html">John Martin</a></h4>
+										<p>Consultant</p>
+									</div>
+								</div>
 							</div>
 						</div>
-					</div>
-				</div>
-                        {/* <div className="row row-gutter-y-40 d-flex justify-content-center pt-5">
+						{/* <div className="row row-gutter-y-40 d-flex justify-content-center pt-5">
                             <div className="col-lg-8 col-xl-8">
                                 <div className="search-popup-content-x">
                                     <form action="#">
@@ -100,11 +168,11 @@ const StrukturOrganisasi = () => {
                                 </div>
                             </div>
                         </div> */}
-                    </div>
-                </section>
-            </div>
-        </>
-    )
+					</div>
+				</section>
+			</div>
+		</>
+	)
 }
 
 export default StrukturOrganisasi
