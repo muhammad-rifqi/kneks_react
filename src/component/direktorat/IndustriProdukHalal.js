98066b107f0b010ac16eb42d33e08789c0295cbb
Rizsky Darmawan <darmawanrizsky@gmail.com>
2024-08-10 20:18:46 +0700

    up
    up

--- src/component/direktorat/IndustriProdukHalal.js
+++ src/component/direktorat/IndustriProdukHalal.js
@@ -2,51 +2,53 @@ import React from "react";
 const IndustriProdukHalal = () => {
     return (
         <>
-        <div className="page-wrapper">
-            
-            <section className="page-banner">
-                <div className="container">
-                    <div className="page-banner-title">
-                        <h3>Industri Produk Halal</h3>
+            <div className="page-wrapper">
+
+                <section className="page-banner">
+                    <div className="container">
+                        <div className="page-banner-title">
+                            <h3>Direktorat</h3>
+                        </div>
                     </div>
-                </div>
-            </section>
-            <section className="about-one-section">
-                <div className="container">
-                    <div className="row row-gutter-y-40">
-                        <div className="col-lg-12 col-xl-6">
-                            <div className="about-one-inner">
-                                <div className="section-tagline">
-                                    Our introductions
+                </section>
+                <section className="about-one-section">
+                    <div className="container">
+                        <div className="row row-gutter-y-40">
+                            <div className="col-lg-12 ">
+                                <div className="event-details-inner-box text-center">
+                                    <img src="assets/image/diph.svg" className="img-fluid" alt="img-173" />
+
                                 </div>
-                                <h2 className="section-title">Welcome to Mexican City Municipal Council</h2>
-                                <p>Aliquam viverra arcu. Donec aliquet blandit enim feugiat. Suspendisse id quam sed eros tincidunt luctus sit amet eu nibh egestas tempus turpis, sit amet mattis magna varius non.</p>
-                                <h5 className="about-one-inner-text">Denounce with righteous indignation and dislike men who are so beguiled & demoralized our power.</h5>
-                                <div className="row row-gutter-y-30">
-                                    <div className="col-xl-6 col-lg-6 col-md-6">
-                                        <div className="about-one-card">
-                                            <div className="about-one-card-number">01</div>
-                                            <div className="about-one-card-content"><h5>Going Above and Beyond</h5></div>
+                                <div className="about-one-inner-x">
+                                    <h2 className="section-title text-center">Direktorat Industri Produk Halal</h2>
+                                    <p>Deskripsi dari direktorat Industri Produk Halal itu apa (Sementara masih menggunakan lorem ipsum) Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                                    <h5 className="about-one-inner-text-x">Divisi</h5>
+                                    <p>Lanjutan deskripsi dari direktorat ini Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                    <div className="row row-gutter-y-30">
+                                        <div className="col-xl-6 col-lg-6 col-md-6">
+                                            <div className="about-one-card">
+                                                <div className="about-one-card-number">01</div>
+                                                {/* <div className="about-one-card-content"><h5>Going Above and Beyond</h5></div> */}
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="col-xl-6 col-lg-6 col-md-6">
-                                        <div className="about-one-card">
-                                            <div className="about-one-card-number">02</div>
-                                            <div className="about-one-card-content"><h5>Committed to People First</h5></div>
+                                        <div className="col-xl-6 col-lg-6 col-md-6">
+                                            <div className="about-one-card">
+                                                <div className="about-one-card-number">02</div>
+                                                {/* <div className="about-one-card-content"><h5>Committed to People First</h5></div> */}
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="col-lg-12 col-xl-6">
-                            <div className="about-one-image">
-                                <img src="assets/image/shapes/shape-1.png" className="floated-image-one" alt="img-58" />
-                                <img src="assets/image/gallery/about-7.jpg" alt="img-59" className="img-fluid" />
+                            <div className="col-lg-12 col-xl-6">
+                                <div className="about-one-image">
+                                    <img src="assets/image/shapes/shape-1.png" className="floated-image-one" alt="img-58" />
+                                    <img src="assets/image/gallery/about-7.jpg" alt="img-59" className="img-fluid" />
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </section>
+                </section>
             </div>
         </>
     )
--- src/component/home/Section11.js
+++ src/component/home/Section11.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "react-router-dom";
 const Section11 = () => {
 	return (
 		<section className="sm-section">
@@ -36,17 +37,136 @@ const Section11 = () => {
 					</div >
 
 				</div>
-				<div className="sm-box pt-5">
+				<div className="sm-box pt-5 pb-5">
 					<div className="section-title-box text-center">
 
 						<h2 className="section-title">Instansi Terkait</h2>
 						<div className="section-text-b "><p className="text-white ">
 							KNEKS beranggotakan 3 Menteri Koordinator, 7 Menteri, 3 Ketua lembaga pemerintah, <br /> Ketua Umum MUI dan Ketua Umum KADIN, dengan Menteri Keuangan merangkap sebagai Sekretaris.
 						</p>
-							<img src="assets/image/instansi.png" className="img-fluid pt-5" alt="img-97" />
+
 						</div>
 					</div>
 				</div>
+
+				<div className="row">
+					<div class="col-6 col-md-4 col-lg-2 pb-3">
+						<Link to={`https://www.kemenkeu.go.id/`} target="_blank" class="component-service d-block ">
+							<div className="service-image ">
+								<img src="assets/image/kemenkeu.png" className="img-fluid " alt="kemenkeu" />
+							</div>
+						</Link>
+					</div>
+					<div class="col-6 col-md-4 col-lg-2 pb-3">
+						<Link to={`https://www.ekon.go.id/`} target="_blank" class="component-service  d-block">
+							<div className="service-image">
+								<img src="assets/image/instansi2.png" className="img-fluid" alt="kementriang bidang perekonomian republik indonesia" />
+							</div>
+						</Link>
+					</div>
+					<div class="col-6 col-md-4 col-lg-2 pb-3">
+						<Link to={`https://www.kemenkopmk.go.id/`} target="_blank" class="component-service  d-block">
+							<div className="service-image">
+								<img src="assets/image/instansi32.png" className="img-fluid" alt="kemenko pmk" />
+							</div>
+						</Link>
+					</div>
+					<div class="col-6 col-md-4 col-lg-2 pb-3">
+						<Link to={`https://www.maritim.go.id/`} target="_blank" class="component-service d-block">
+							<div className="service-image">
+								<img src="assets/image/instansi41.png" className="img-fluid" alt="maritim" />
+							</div>
+						</Link>
+					</div>
+					<div class="col-6 col-md-4 col-lg-2 pb-3">
+						<Link to={`https://www.kemenag.go.id/`} target="_blank" class="component-service d-block">
+							<div className="service-image">
+								<img src="assets/image/instansi5.png" className="img-fluid" alt="kemenag" />
+							</div>
+						</Link>
+					</div>
+
+					<div class="col-6 col-md-4 col-lg-2 pb-3">
+						<Link to={`https://www.kemendag.go.id/`} target="_blank" class="component-service d-block">
+							<div className="service-image">
+								<img src="assets/image/instansi7.png" className="img-fluid" alt="kemendag" />
+							</div>
+						</Link>
+					</div>
+					<div class="col-6 col-md-4 col-lg-2 pb-3">
+						<Link to={`https://bappenas.go.id/`} target="_blank" class="component-service d-block">
+							<div className="service-image">
+								<img src="assets/image/instansi8.png" className="img-fluid" alt="bappenas" />
+							</div>
+						</Link>
+					</div>
+					<div class="col-6 col-md-4 col-lg-2 pb-3">
+						<Link to={`https://bumn.go.id/`} target="_blank" class="component-service d-block">
+							<div className="service-image">
+								<img src="assets/image/instansi9.png" className="img-fluid" alt="bumn" />
+							</div>
+						</Link>
+					</div>
+					<div class="col-6 col-md-4 col-lg-2 pb-3">
+						<Link to={`https://kemenkopukm.go.id/`} target="_blank" class="component-service d-block">
+							<div className="service-image">
+								<img src="assets/image/instansi10.png" className="img-fluid" alt="kemenkopukm" />
+							</div>
+						</Link>
+					</div>
+					<div class="col-6 col-md-4 col-lg-2 pb-3">
+						<Link to={`https://kemenparekraf.go.id/`} target="_blank" class="component-service d-block">
+							<div className="service-image">
+								<img src="assets/image/instansi11.png" className="img-fluid" alt="kemenparekraf" />
+							</div>
+						</Link>
+					</div>
+
+
+					<div class="col-6 col-md-4 col-lg-2 pb-3">
+						<Link to={`https://mui.or.id/`} target="_blank" class="component-service d-block">
+							<div className="service-image">
+								<img src="assets/image/instansi15.png" className="img-fluid" alt="mui" />
+							</div>
+						</Link>
+					</div>
+					<div class="col-6 col-md-4 col-lg-2 pb-3">
+						<Link to={`https://kadin.id/`} target="_blank" class="component-service d-block">
+							<div className="service-image">
+								<img src="assets/image/instansi17.png" className="img-fluid" alt="kadin" />
+							</div>
+						</Link>
+					</div>
+					<div class="col-6 col-md-4 col-lg-3 pb-3">
+						<Link to={`https://bi.go.id/`} target="_blank" class="component-service d-block">
+							<div className="service-image">
+								<img src="assets/image/instansi13.png" className="img-fluid" alt="bi" />
+							</div>
+						</Link>
+					</div>
+					<div class="col-6 col-md-4 col-lg-3 pb-3">
+						<Link to={`https://lps.go.id/`} target="_blank" class="component-service d-block">
+							<div className="service-image">
+								<img src="assets/image/instansi14.png" className="img-fluid" alt="lps" />
+							</div>
+						</Link>
+					</div>
+					<div class="col-6 col-md-4 col-lg-3 pb-3">
+						<Link to={`https://www.kemenperin.go.id/`} target="_blank" class="component-service d-block">
+							<div className="service-image">
+								<img src="assets/image/instansi6.png" className="img-fluid" alt="kemenperin" />
+							</div>
+						</Link>
+					</div>
+					<div class="col-6 col-md-4 col-lg-3 pb-3">
+						<Link to={`https://ojk.go.id/`} target="_blank" class="component-service d-block">
+							<div className="service-image">
+								<img src="assets/image/instansi12.png" className="img-fluid" alt="ojk" />
+							</div>
+						</Link>
+					</div>
+				</div>
+
 
 			</div >
 		</section >
