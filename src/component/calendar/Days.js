f88b04427f2164ce87409def6eef4e11b92d21ba
Rizsky Darmawan <darmawanrizsky@gmail.com>
2024-08-16 22:50:36 +0700

    up
    up

--- src/component/calendar/Days.js
+++ src/component/calendar/Days.js
@@ -5,18 +5,18 @@ export default function Days({ day, rowIdx }) {
 
     function getCurrentDayClass() {
         return day.format("DD-MM-YY") === dayjs().format("DD-MM-YY")
-            ? "bg-primary text-white rounded-circle d-inline-block text-center"
+            ? "bg-primary text-white rounded-circle p-1 text-center"
             : "";
     }
     return (
         <div className="border border-secondary d-flex flex-column">
             <header className="d-flex flex-column align-items-center">
                 {rowIdx === 0 && (
-                    <p className="small mt-1">
+                    <p className="small ">
                         {day.format("ddd").toUpperCase()}
                     </p>
                 )}
-                <p className={`small py-1 my-1 text-center ${getCurrentDayClass()}`}>
+                <p className={`small  text-center ${getCurrentDayClass()}`}>
                     {day.format("DD")}
                 </p>
             </header>
