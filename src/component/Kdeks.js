4c70db55c5f92aebff0963585296b7f1aaa80723
rizskydarmawann <70370415+rizskydarmawann@users.noreply.github.com>
2024-08-16 15:07:23 +0700

    up
    up

--- src/component/Kdeks.js
+++ src/component/Kdeks.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import isiItemsBerita from "../component/dumy/dataBerita";
 // import OwlCarousel from 'react-owl-carousel';
 import Swiper from 'swiper/bundle';
+import Swipers from 'swiper/bundle';
 import 'swiper/swiper-bundle.css';
 const Kdeks = () => {
     const [rows, setItems] = useState([]);
@@ -10,51 +11,95 @@ const Kdeks = () => {
 
     // untukmengeloladatasebelumdiloop
     useEffect(() => {
+        if (document.querySelector('.swiper-kdeks-agenda')) {
+            // var mySwiper = new Swiper('.swiper-kdeks', {
+            // pengaturan Swiper
+
+            const swipers = new Swipers('.swiper-kdeks-agenda', {
+                // pengaturan Swiper
+                loop: true,
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                },
+                autoplay: {
+                    delay: 2500,
+                    disableOnInteraction: false,
+                },
+                breakpoints: {
+                    640: {
+                        slidesPerView: 2,
+                        spaceBetween: 20,
+                    },
+                    768: {
+                        slidesPerView: 3,
+                        spaceBetween: 30,
+                    },
+                    1024: {
+                        slidesPerView: 4,
+                        spaceBetween: 20,
+                    },
+                },
+            });
+            // Cleanup function to destroy Swiper instance
+            return () => {
+                if (swipers) {
+                    swipers.destroy(true, true);
+                }
+            };
+        }
+    },[])
+    useEffect(() => {
         const isian = isiItemsBerita();
         setItems(isian);
         // alert(items.length);
         if (document.querySelector('.swiper-kdeks')) {
             // var mySwiper = new Swiper('.swiper-kdeks', {
-                // pengaturan Swiper
-        
-        const swiper = new Swiper('.swiper-kdeks', {
             // pengaturan Swiper
-            loop: true,
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-            autoplay: {
-                delay: 2500,
-                disableOnInteraction: false,
-            },
-            breakpoints: {
-                640: {
-                    slidesPerView: 2,
-                    spaceBetween: 20,
+
+            const swiper = new Swiper('.swiper-kdeks', {
+                // pengaturan Swiper
+                loop: true,
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
                 },
-                768: {
-                    slidesPerView: 3,
-                    spaceBetween: 30,
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
                 },
-                1024: {
-                    slidesPerView: 4,
-                    spaceBetween: 20,
+                autoplay: {
+                    delay: 2500,
+                    disableOnInteraction: false,
                 },
-            },
-        });
-          // Cleanup function to destroy Swiper instance
-          return () => {
-            if (swiper) {
-                swiper.destroy(true, true);
-            }
-        };
-    }
-      
+                breakpoints: {
+                    640: {
+                        slidesPerView: 2,
+                        spaceBetween: 20,
+                    },
+                    768: {
+                        slidesPerView: 3,
+                        spaceBetween: 30,
+                    },
+                    1024: {
+                        slidesPerView: 4,
+                        spaceBetween: 20,
+                    },
+                },
+            });
+            // Cleanup function to destroy Swiper instance
+            return () => {
+                if (swiper) {
+                    swiper.destroy(true, true);
+                }
+            };
+        }
+       
+
 
 
     }, []);
@@ -135,10 +180,10 @@ const Kdeks = () => {
                         <h2 className="section-title">Berita Dan Kegiatan</h2>
                     </div>
                     <div className="portfolio-content container-fluid">
-                    <div className="swiper swiper-kdeks">
+                        <div className="swiper swiper-kdeks">
                             <div className="swiper-wrapper">
 
-                                {rows.map((item, i) => (
+                                {rows.slice(0,5).map((item, i) => (
                                     <div className="swiper-slide" key={i}>
                                         <div className="berita-card-kdeks shadow" >
                                             <div className="berita-card-imgbox-kdeks">
@@ -164,6 +209,89 @@ const Kdeks = () => {
                             {/* <div className="swiper-pagination"></div> */}
                             <div className="swiper-button-next"></div>
                             <div className="swiper-button-prev"></div>
+                        </div>
+                    </div>
+                </section>
+                <section className="funfact-section">
+                    <div className="container">
+                        <div className="funfact-box">
+                            <div className="section-title-box text-center">
+                                <h2 className="section-title">Agenda</h2>
+                            </div>
+                        </div>
+                        <div className="row ">
+                            <div className="swiper swiper-kdeks-agenda">
+                                <div className="swiper-wrapper">
+
+                                    <div className="col-lg-3 col-md-6 swiper-slide">
+                                        <div className="card shadow p-3 mb-5 rounded" style={{ background: `#146AA4`, color: `#ffffff` }}>
+                                            <div className="card-header" style={{ borderBottom: `1px solid #ffffff`, fontSize: `18px`, paddingBottom: `15px`, background: `#146AA4` }}>Webinar Keuangan</div>
+                                            <div className="card-body">
+                                                <div className="card-text">24/05/2024 - 24/05/2024</div>
+                                                <div className="card-text">12:00 - 14:00 WIP</div>
+
+                                            </div>
+                                            <div className="card-footer text-end" style={{ borderTop: `none`, background: `#146AA4`, color: `#ffffff` }}>
+                                                <i className="fa-solid fa-calendar "></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-3 col-md-6 swiper-slide">
+                                        <div className="card shadow p-3 mb-5 rounded" style={{ background: `#146AA4`, color: `#ffffff` }}>
+                                            <div className="card-header" style={{ borderBottom: `1px solid #ffffff`, fontSize: `18px`, paddingBottom: `15px`, background: `#146AA4` }}>Webinar Keuangan</div>
+                                            <div className="card-body">
+                                                <div className="card-text">24/05/2024 - 24/05/2024</div>
+                                                <div className="card-text">12:00 - 14:00 WIP</div>
+
+                                            </div>
+                                            <div className="card-footer text-end" style={{ borderTop: `none`, background: `#146AA4`, color: `#ffffff` }}>
+                                                <i className="fa-solid fa-calendar "></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-3 col-md-6 swiper-slide">
+                                        <div className="card shadow p-3 mb-5 rounded" style={{ background: `#146AA4`, color: `#ffffff` }}>
+                                            <div className="card-header" style={{ borderBottom: `1px solid #ffffff`, fontSize: `18px`, paddingBottom: `15px`, background: `#146AA4` }}>Webinar Keuangan</div>
+                                            <div className="card-body">
+                                                <div className="card-text">24/05/2024 - 24/05/2024</div>
+                                                <div className="card-text">12:00 - 14:00 WIP</div>
+
+                                            </div>
+                                            <div className="card-footer text-end" style={{ borderTop: `none`, background: `#146AA4`, color: `#ffffff` }}>
+                                                <i className="fa-solid fa-calendar "></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-3 col-md-6 swiper-slide">
+                                        <div className="card shadow p-3 mb-5 rounded" style={{ background: `#146AA4`, color: `#ffffff` }}>
+                                            <div className="card-header" style={{ borderBottom: `1px solid #ffffff`, fontSize: `18px`, paddingBottom: `15px`, background: `#146AA4` }}>Webinar Keuangan</div>
+                                            <div className="card-body">
+                                                <div className="card-text">24/05/2024 - 24/05/2024</div>
+                                                <div className="card-text">12:00 - 14:00 WIP</div>
+
+                                            </div>
+                                            <div className="card-footer text-end" style={{ borderTop: `none`, background: `#146AA4`, color: `#ffffff` }}>
+                                                <i className="fa-solid fa-calendar "></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-3 col-md-6 swiper-slide">
+                                        <div className="card shadow p-3 mb-5 rounded" style={{ background: `#146AA4`, color: `#ffffff` }}>
+                                            <div className="card-header" style={{ borderBottom: `1px solid #ffffff`, fontSize: `18px`, paddingBottom: `15px`, background: `#146AA4` }}>Webinar Keuangan</div>
+                                            <div className="card-body">
+                                                <div className="card-text">24/05/2024 - 24/05/2024</div>
+                                                <div className="card-text">12:00 - 14:00 WIP</div>
+
+                                            </div>
+                                            <div className="card-footer text-end" style={{ borderTop: `none`, background: `#146AA4`, color: `#ffffff` }}>
+                                                <i className="fa-solid fa-calendar "></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="swiper-button-next"></div>
+                                <div className="swiper-button-prev"></div>
+                            </div>
                         </div>
                     </div>
                 </section>
