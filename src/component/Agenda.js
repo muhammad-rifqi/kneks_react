50724423c1c1e9acbd84cc5ebfe7d3c3785714db
rizskydarmawann <70370415+rizskydarmawann@users.noreply.github.com>
2024-08-21 11:30:53 +0700

    up
    up

--- src/component/Agenda.js
+++ src/component/Agenda.js
@@ -1,5 +1,36 @@
 import React, { useState } from "react";
+import FullCalendar, { formatDate } from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
 const Agenda = () => {
+    const [currentEvents, setCurrentEvents] = useState([]);
+
+    const handleDateClick = (selected) => {
+        const title = prompt("Please enter a new title for your event");
+        const calendarApi = selected.view.calendar;
+        calendarApi.unselect();
+
+        if (title) {
+            calendarApi.addEvent({
+                id: `${selected.dateStr}-${title}`,
+                title,
+                start: selected.startStr,
+                end: selected.endStr,
+                allDay: selected.allDay,
+            });
+        }
+    };
+    const handleEventClick = (selected) => {
+        if (
+            window.confirm(
+                `Are you sure you want to delete the event '${selected.event.title}'`
+            )
+        ) {
+            selected.event.remove();
+        }
+    };
     return (
         <>
             <div className="page-wrapper">
@@ -12,9 +43,64 @@ const Agenda = () => {
                     </div>
                 </section>
                 <section className="about-one-section">
-                    <div className="container">
-                        
-                        
+                    <div className="container-sm">
+
+                        <div className="row">
+                            <div className="col-lg-3">
+
+                            </div>
+                            <div className="col-lg-9">
+                                <FullCalendar
+                                    height="75vh"
+                                    plugins={[
+                                        dayGridPlugin,
+                                        timeGridPlugin,
+                                        interactionPlugin,
+                                        listPlugin,
+                                    ]}
+                                    headerToolbar={{
+                                        left: "title",
+                                        // center: "today",
+                                        right: "prev,dayGridMonth,next",
+                                    }}
+                                    initialView="dayGridMonth"
+                                    editable={true}
+                                    selectable={true}
+                                    selectMirror={true}
+                                    dayMaxEvents={true}
+                                    select={handleDateClick}
+                                    eventClick={handleEventClick}
+                                    eventsSet={(events) => setCurrentEvents(events)}
+                                    initialEvents={[
+                                        {
+                                            id: "12315",
+                                            title: "All-day event",
+                                            date: "2024-08-23",
+                                        },
+                                        {
+                                            id: "5123",
+                                            title: "Timed event",
+                                            date: "2024-08-21",
+                                        },
+                                        {
+                                            id: "5124",
+                                            title: "Timed event",
+                                            date: "2024-08-25",
+                                        },
+                                        {
+                                            id: "5124",
+                                            title: "Timed event",
+                                            date: "2024-08-25",
+                                        },
+                                        {
+                                            id: "5124",
+                                            title: "Timed event",
+                                            date: "2024-08-25",
+                                        },
+                                    ]}
+                                />
+                            </div>
+                        </div>
                     </div>
                 </section>
             </div>
