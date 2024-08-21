34e641ff76c8ef4f11c25fa2e58d2ed7fd07eb8f
rizskydarmawann <70370415+rizskydarmawann@users.noreply.github.com>
2024-08-21 14:56:41 +0700

    up
    up

--- src/component/Agenda.js
+++ src/component/Agenda.js
@@ -1,40 +1,29 @@
 import React, { useState } from "react";
-import FullCalendar, { formatDate } from "@fullcalendar/react";
+import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import listPlugin from "@fullcalendar/list";
+import EventDetailModal from "../component/modal/AgendaModal"; // Event detail modal
+
 const Agenda = () => {
     const [currentEvents, setCurrentEvents] = useState([]);
+    const [showDetailModal, setShowDetailModal] = useState(false);
+    const [selectedEvent, setSelectedEvent] = useState(null);
 
     const handleDateClick = (selected) => {
-        const title = prompt("Please enter a new title for your event");
-        const calendarApi = selected.view.calendar;
-        calendarApi.unselect();
+        // Handle date click to add events (if necessary)
+        
+    };
 
-        if (title) {
-            calendarApi.addEvent({
-                id: `${selected.dateStr}-${title}`,
-                title,
-                start: selected.startStr,
-                end: selected.endStr,
-                allDay: selected.allDay,
-            });
-        }
-    };
     const handleEventClick = (selected) => {
-        if (
-            window.confirm(
-                `Are you sure you want to delete the event '${selected.event.title}'`
-            )
-        ) {
-            selected.event.remove();
-        }
+        setSelectedEvent(selected.event);
+        setShowDetailModal(true);
     };
+
     return (
         <>
             <div className="page-wrapper">
-
                 <section className="page-banner">
                     <div className="container">
                         <div className="page-banner-title">
@@ -44,23 +33,14 @@ const Agenda = () => {
                 </section>
                 <section className="about-one-section">
                     <div className="container-sm">
-
                         <div className="row">
-                            <div className="col-lg-3">
-
-                            </div>
+                            <div className="col-lg-3"></div>
                             <div className="col-lg-9">
                                 <FullCalendar
                                     height="75vh"
-                                    plugins={[
-                                        dayGridPlugin,
-                                        timeGridPlugin,
-                                        interactionPlugin,
-                                        listPlugin,
-                                    ]}
+                                    plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
                                     headerToolbar={{
                                         left: "title",
-                                        // center: "today",
                                         right: "prev,dayGridMonth,next",
                                     }}
                                     initialView="dayGridMonth"
@@ -72,40 +52,25 @@ const Agenda = () => {
                                     eventClick={handleEventClick}
                                     eventsSet={(events) => setCurrentEvents(events)}
                                     initialEvents={[
-                                        {
-                                            id: "12315",
-                                            title: "All-day event",
-                                            date: "2024-08-23",
-                                        },
-                                        {
-                                            id: "5123",
-                                            title: "Timed event",
-                                            date: "2024-08-21",
-                                        },
-                                        {
-                                            id: "5124",
-                                            title: "Timed event",
-                                            date: "2024-08-25",
-                                        },
-                                        {
-                                            id: "5124",
-                                            title: "Timed event",
-                                            date: "2024-08-25",
-                                        },
-                                        {
-                                            id: "5124",
-                                            title: "Timed event",
-                                            date: "2024-08-25",
-                                        },
+                                        { id: "12315", title: "All-day event", date: "2024-08-23" },
+                                        { id: "5123", title: "Timed event", date: "2024-08-21" },
+                                        { id: "5124", title: "Timed event", date: "2024-08-25" },
+                                        { id: "5125", title: "Timed e", date: "2024-08-25" },
+                                        { id: "5126", title: "Timed ez", date: "2024-08-25" },
                                     ]}
                                 />
                             </div>
                         </div>
                     </div>
                 </section>
+                <EventDetailModal
+                    show={showDetailModal}
+                    handleClose={() => setShowDetailModal(false)}
+                    event={selectedEvent}
+                />
             </div>
         </>
-    )
-}
+    );
+};
 
-export default Agenda
+export default Agenda;
