53fe1286a41f755d15ca591d7c78d37c3501acb0
rizskydarmawann <70370415+rizskydarmawann@users.noreply.github.com>
2024-10-31 10:14:26 +0700

    up

--- src/component/dumy/dataKota.js
+++ src/component/dumy/dataKota.js
@@ -58,6 +58,41 @@ const dataKota = () => {
             skFile: "assets/image/SkKdeks/SK_KDEKS_Lampung.pdf",
             kode: "lampung"
         },
+        {
+            title: "Bangka Belitung",
+            tanggal: "20 Juni 2024",
+            foto: "assets/image/logoKdeks/KDEKS_Babel_Logo.jpg",
+            skFile: "assets/image/SkKdeks/SK_KDEKS_Lampung.pdf",
+            kode: "babel"
+        },
+        {
+            title: "Gorontalo",
+            tanggal: "20 Juni 2024",
+            foto: "assets/image/logoKdeks/KDEKS_Gorontalo_Logo.png",
+            skFile: "assets/image/SkKdeks/SK_KDEKS_Lampung.pdf",
+            kode: "gorontalo"
+        },
+        {
+            title: "Jawa Timur",
+            tanggal: "20 Juni 2024",
+            foto: "assets/image/logoKdeks/KDEKS_Jawa_Timur.png",
+            skFile: "assets/image/SkKdeks/SK_KDEKS_Lampung.pdf",
+            kode: "jatim"
+        },
+        {
+            title: "Jawa tengah",
+            tanggal: "20 Juni 2024",
+            foto: "assets/image/logoKdeks/KDEKS_Jawa_Tengah_Logo_1.png",
+            skFile: "assets/image/SkKdeks/SK_KDEKS_Lampung.pdf",
+            kode: "jateng"
+        },
+        {
+            title: "Kalimantan Timur",
+            tanggal: "20 Juni 2024",
+            foto: "assets/image/logoKdeks/KDEKS_Kalimantan_Timur_Logo.jpg",
+            skFile: "assets/image/SkKdeks/SK_KDEKS_Lampung.pdf",
+            kode: "kaltim"
+        },
 
     ]
 };
