03fd7386b7ac39fc84f338c459b2daf18a62bc09
Muhammad Rifqi <muhammad45rifki@gmail.com>
2024-12-10 11:59:38 +0700

    update

--- src/component/home/Section9.js
+++ src/component/home/Section9.js
@@ -1,4 +1,4 @@
-// import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 
@@ -31,55 +31,26 @@ const Section9 = () => {
 			},
 		},
 	};
-	const data = [
-		{
-			icon: '💼', // Replace with actual icon component or image path
-			title: 'Tingkat Pengangguran Terbuka',
-			value: '4,82',
-			unit: 'Persen',
-			date: 'Februari 2024',
-		},
-		{
-			icon: '💰', // Replace with actual icon component or image path
-			title: 'Gini Rasio',
-			value: '0,379',
-			date: 'Maret 2024',
-		},
-		{
-			icon: '🏠', // Replace with actual icon component or image path
-			title: 'IPM (UHH LF SP2020)',
-			value: '74,39',
-			date: '2023',
-		},
-		{
-			icon: '🌐', // Replace with actual icon component or image path
-			title: 'Nilai Ekspor',
-			value: '24.421,6',
-			unit: 'Juta US$',
-			date: 'Oktober 2024',
-		},
-		{
-			icon: '🌐', // Replace with actual icon component or image path
-			title: 'Nilai Impor',
-			value: '21.938,3',
-			unit: 'Juta US$',
-			date: 'Oktober 2024',
-		},
-		{
-			icon: '🌐', // Replace with actual icon component or image path
-			title: 'Nilai Impor',
-			value: '21.938,3',
-			unit: 'Juta US$',
-			date: 'Oktober 2024',
-		},
-		{
-			icon: '🌐', // Replace with actual icon component or image path
-			title: 'Nilai Impor',
-			value: '21.938,3',
-			unit: 'Juta US$',
-			date: 'Oktober 2024',
-		},
-	];
+
+	const [data, setData] = useState([]);
+	useEffect(() => {
+		fetch(process.env.REACT_APP_API_URL + '/statistics')
+			.then(resp => resp.json())
+			.then((rows) => {
+				const arr = [];
+				rows.forEach((element) => {
+					const ddd = {
+						icon: '🌐',
+						title: element?.title,
+						value: element?.amount,
+						unit: 'Data',
+						date: element?.date_created.split('T')[0],
+					}
+					arr.push(ddd)
+				});
+				setData(arr)
+			})
+	})
 
 	return (
 		<section className='funfact-section'>
@@ -92,21 +63,21 @@ const Section9 = () => {
 					</div>
 				</div>
 				<OwlCarousel className="owl-themes" {...options} data-aos="fade-down-left" >
-					
-						{data.map((item, index) => (
-							<div className="row align-items-center justify-content-center">
+
+					{data.map((item, index) => (
+						<div className="row align-items-center justify-content-center">
 							<div className="col-md-12 text-center mb-3 " key={index}>
 								<div className="card bg-light border-0 shadow p-3">
 									{/* Replace with your actual icon component or image */}
 									{/* <i className="fas fa-briefcase fa-2x mb-3"></i> */}
-									<h5 style={{fontSize:'16px'}} className="card-title mb-0">{item.title}</h5>
+									<h5 style={{ fontSize: '16px' }} className="card-title mb-0">{item.title}</h5>
 									<h3 className="card-text mb-0">{item.value}</h3>
 									<p className="card-text text-muted">{item.date}</p>
 								</div>
 							</div>
-							</div>
-						))}
-					
+						</div>
+					))}
+
 				</OwlCarousel>
 				{/* <Tabs
                     activeKey={activeTab}
