c1945d22f0a2ca26d5c1cf4564f668b272b07b52
rizskydarmawann <70370415+rizskydarmawann@users.noreply.github.com>
2024-12-10 11:54:38 +0700

    up

--- src/component/home/Section9.js
+++ src/component/home/Section9.js
@@ -1,21 +1,27 @@
-// import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import SkeletonCardBerita from "../skeleton/CardBerita";
+import axios from 'axios';
 import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 
 import 'owl.carousel/dist/assets/owl.theme.default.css';
 import { useTranslation } from "react-i18next";
-
+import Swal from "sweetalert2";
+import dayjs from 'dayjs';
+import 'dayjs/locale/id';
 
 
 const Section9 = () => {
 	const { t } = useTranslation();
+	dayjs.locale('id');
+	const [loading, setLoading] = useState(true);
+	const [posts, setPosts] = useState([]);
 
 	const options = {
 		items: 1,
 		margin: 30,
 		loop: true, // Disable looping since we only have one slide
 		navigation: true,
-		// navText: ['<i className="fa-arrow-left-long"></i>', '<i className="fa-solid fa-arrow-right-long"></i>'],
 		autoplay: true,
 		autoplayTimeout: 4000,
 		dots: false,
@@ -31,55 +37,79 @@ const Section9 = () => {
 			},
 		},
 	};
-	const data = [
-		{
-			icon: '💼', // Replace with actual icon component or image path
-			title: 'Tingkat Pengangguran Terbuka',
-			value: '4,82',
-			unit: 'Persen',
-			date: 'Februari 2024',
-		},
-		{
-			icon: '💰', // Replace with actual icon component or image path
-			title: 'Gini Rasio',
-			value: '0,379',
-			date: 'Maret 2024',
-		},
-		{
-			icon: '🏠', // Replace with actual icon component or image path
-			title: 'IPM (UHH LF SP2020)',
-			value: '74,39',
-			date: '2023',
-		},
-		{
-			icon: '🌐', // Replace with actual icon component or image path
-			title: 'Nilai Ekspor',
-			value: '24.421,6',
-			unit: 'Juta US$',
-			date: 'Oktober 2024',
-		},
-		{
-			icon: '🌐', // Replace with actual icon component or image path
-			title: 'Nilai Impor',
-			value: '21.938,3',
-			unit: 'Juta US$',
-			date: 'Oktober 2024',
-		},
-		{
-			icon: '🌐', // Replace with actual icon component or image path
-			title: 'Nilai Impor',
-			value: '21.938,3',
-			unit: 'Juta US$',
-			date: 'Oktober 2024',
-		},
-		{
-			icon: '🌐', // Replace with actual icon component or image path
-			title: 'Nilai Impor',
-			value: '21.938,3',
-			unit: 'Juta US$',
-			date: 'Oktober 2024',
-		},
-	];
+
+	useEffect(() => {
+		// Function to fetch posts
+		const fetchPosts = async () => {
+			setLoading(true);
+			try {
+				const url = process.env.REACT_APP_API_URL;
+				const endpoint = process.env.REACT_APP_API_DATA;
+				const response = await axios.get(`${url}${endpoint}`);
+				setPosts(response.data);
+				console.log(response.data)
+			} catch (err) {
+				Swal.fire({
+					icon: "error",
+					title: "Oops...",
+					text: err,
+				});
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		fetchPosts(); // Call fetchPosts function when component mounts
+	}, []);
+	// const data = [
+	// 	{
+	// 		icon: '💼', // Replace with actual icon component or image path
+	// 		title: 'Tingkat Pengangguran Terbuka',
+	// 		value: '4,82',
+	// 		unit: 'Persen',
+	// 		date: 'Februari 2024',
+	// 	},
+	// 	{
+	// 		icon: '💰', // Replace with actual icon component or image path
+	// 		title: 'Gini Rasio',
+	// 		value: '0,379',
+	// 		date: 'Maret 2024',
+	// 	},
+	// 	{
+	// 		icon: '🏠', // Replace with actual icon component or image path
+	// 		title: 'IPM (UHH LF SP2020)',
+	// 		value: '74,39',
+	// 		date: '2023',
+	// 	},
+	// 	{
+	// 		icon: '🌐', // Replace with actual icon component or image path
+	// 		title: 'Nilai Ekspor',
+	// 		value: '24.421,6',
+	// 		unit: 'Juta US$',
+	// 		date: 'Oktober 2024',
+	// 	},
+	// 	{
+	// 		icon: '🌐', // Replace with actual icon component or image path
+	// 		title: 'Nilai Impor',
+	// 		value: '21.938,3',
+	// 		unit: 'Juta US$',
+	// 		date: 'Oktober 2024',
+	// 	},
+	// 	{
+	// 		icon: '🌐', // Replace with actual icon component or image path
+	// 		title: 'Nilai Impor',
+	// 		value: '21.938,3',
+	// 		unit: 'Juta US$',
+	// 		date: 'Oktober 2024',
+	// 	},
+	// 	{
+	// 		icon: '🌐', // Replace with actual icon component or image path
+	// 		title: 'Nilai Impor',
+	// 		value: '21.938,3',
+	// 		unit: 'Juta US$',
+	// 		date: 'Oktober 2024',
+	// 	},
+	// ];
 
 	return (
 		<section className='funfact-section'>
@@ -92,21 +122,25 @@ const Section9 = () => {
 					</div>
 				</div>
 				<OwlCarousel className="owl-themes" {...options} data-aos="fade-down-left" >
-					
-						{data.map((item, index) => (
+
+					{posts.length > 0 ? (
+						posts.map((item,index) => (
 							<div className="row align-items-center justify-content-center">
-							<div className="col-md-12 text-center mb-3 " key={index}>
-								<div className="card bg-light border-0 shadow p-3">
-									{/* Replace with your actual icon component or image */}
-									{/* <i className="fas fa-briefcase fa-2x mb-3"></i> */}
-									<h5 style={{fontSize:'16px'}} className="card-title mb-0">{item.title}</h5>
-									<h3 className="card-text mb-0">{item.value}</h3>
-									<p className="card-text text-muted">{item.date}</p>
+								<div className="col-md-12 text-center mb-3 " key={index}>
+									<div className="card bg-light border-0 shadow p-3">
+										{/* Replace with your actual icon component or image */}
+										{/* <i className="fas fa-briefcase fa-2x mb-3"></i> */}
+										<h5 style={{ fontSize: '16px' }} className="card-title mb-0">{item.title}</h5>
+										<h3 className="card-text mb-0">{item.amount}</h3>
+										<p className="card-text text-muted">{dayjs(item.date_created).format('DD MMMM YYYY')}</p>
+									</div>
 								</div>
 							</div>
-							</div>
-						))}
-					
+						))
+					) : (
+						<p className="text-center">No posts available.</p>
+					)}
+
 				</OwlCarousel>
 				{/* <Tabs
                     activeKey={activeTab}
