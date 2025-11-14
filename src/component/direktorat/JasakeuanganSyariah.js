08e743b4fff448da63f794dcfd9895e0a8872bc0
Muhammad Rifqi <muhammad45rifki@gmail.com>
2025-11-14 18:18:42 +0700

    update

--- src/component/direktorat/JasakeuanganSyariah.js
+++ src/component/direktorat/JasakeuanganSyariah.js
@@ -472,8 +472,7 @@ const JasakeuanganSyariah = () => {
                                             <div className="col-lg-3 col-xl-3" key={item.id}>
                                                 <div className="berita-card">
                                                     <div className="berita-card-imgbox-direktorat ">
-                                                        {/* /${id_dir} */}
-                                                        <a href={`/opini-direktorat/${item.id}/${convertToSlug(item.title)}`}><img src="/assets/image/foto-beritas.png" className="img-fluid" alt={item.title} /></a>
+                                                        <a href={`/opini-direktorat/${item.id}/${convertToSlug(item.title)}/${id_dir}`}><img src="/assets/image/foto-beritas.png" className="img-fluid" alt={item.title} /></a>
                                                     </div>
                                                     <div className="berita-content-direktorat-xs">
 
